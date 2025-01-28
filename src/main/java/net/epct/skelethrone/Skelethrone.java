de9f14f05ff9b6374f00652a03b8fca40423cc70
epct <epct.programming@gmail.com>
2025-01-28 18:23:35 +0100

    Initial commit

---
+++ src/main/java/net/epct/skelethrone/Skelethrone.java
@@ -0,0 +1,17 @@
+package net.epct.skelethrone;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Skelethrone implements ModInitializer {
+	public static final String MOD_ID = "skelethrone";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/epct/skelethrone/SkelethroneClient.java
@@ -0,0 +1,10 @@
+package net.epct.skelethrone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkelethroneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
